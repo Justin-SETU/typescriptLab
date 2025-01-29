c1d44b38ec54e6ecb96ba4757527646ce5ccbe23
Justin-SETU <20110497@mail.wit.ie>
2025-01-29 16:29:48 +0000

    Added type-safety to friend data.

---
+++ src/01-basics.ts
@@ -0,0 +1,55 @@
+interface Friend {
+    name: string;
+    phone: string;
+    age: number;
+}
+
+const friend1 = {
+    name: "Justin Jose",
+    phone: "0890698776",
+    age: 25,
+  };
+  
+  const friend2 = {
+    name: "Bijo Sabu",
+    phone: "086987654",
+    age: 27,
+  };
+  
+  const friends = [friend1, friend2];
+  console.log(friends[1]);
+  
+  //   -------------------
+  const colleague1 = {
+    name: "Joseph Ashin",
+    department: "Engineering",
+    contact: {
+      email: "jash@company.com",
+      extension: 121,
+    },
+  };
+  
+  const colleague2 = {
+    name: "Sivadas M",
+    department: "Cyber Security",
+    contact: {
+      email: "sivam50@company.com",
+      extension: 888,
+    },
+  };
+  
+  const colleague3 = {
+    name: "Joel Babice",
+    department: "HR",
+    contact: {
+      email: "jbs@company.com",
+      extension: 111,
+    },
+  };
+  const colleagues = {
+    current: [colleague1, colleague2, colleague3],
+    former: [],
+  };
+  
+  console.log(colleagues.current[0]);
+  
